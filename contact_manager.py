e4306c8e0107c1322b39d43d14fa687e35c4c288
Daniel Billotte <daniel.billotte@galvanize.com>
2025-03-04 22:31:46 -0700

    removed solution code

--- contact_manager.py
+++ contact_manager.py
@@ -9,33 +9,18 @@ class ContactManager:
         return self._load_contacts_from_file()
 
     def add_contact(self, contact):
-        contacts = self.load_contacts()
-        contacts.append(contact)
-        self._save_contacts_to_file(contacts)
+        pass
 
     def update_contact(self, contact_to_update):
-        contacts = self._load_contacts_from_file()
-        for i, contact in enumerate(contacts):
-            if contact["id"] == contact_to_update["id"]:
-                contacts[i] = contact_to_update
-                break
-        self._save_contacts_to_file(contacts)
+        pass
 
     def delete_contact(self, id_to_delete):
-        contacts = self._load_contacts_from_file()
-        contacts = [
-            contact for contact in contacts if contact["id"] != id_to_delete
-        ]
-        self._save_contacts_to_file(contacts)
+        pass
 
     # these are "private" methods below. They are not meant to be called
-    # from outside the class
+    # from outside the class, but are likely useful in the methods above.
     def _load_contacts_from_file(self):
-        contacts = []
-        with open(self.file, "r") as f:
-            contacts = json.load(f)        
-        return contacts
+        return []
 
     def _save_contacts_to_file(self, contacts):
-        with open(self.file, "w") as f:
-            json.dump(contacts, f)
+        pass

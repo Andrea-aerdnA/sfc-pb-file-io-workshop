d69da3ba3d30b55ad8b757b06d8d8c456963564b
Andrea <andrea.j.huelsnitz@gmail.com>
2026-09-01 16:59:24 -0400

    first upload

--- contact_manager.py
+++ contact_manager.py
@@ -6,7 +6,9 @@ An alternative approach is to build a "Contact" class to represent a contact.
 If you would like to do that as a Bonus exercise, that would be good way
 to practice using OOP and composition!
 """
-
+import io
+import json
+import os
 
 class ContactManager:
     """Class to do CRUD operations on the list of contacts"""
@@ -14,32 +16,70 @@ class ContactManager:
     def __init__(self, file="data.json"):
         self.file = file
         self.contacts = []
+        self.load_contacts()
 
     def load_contacts(self):
-        """Loads contacts from a JSON file and converts them to a list of
-        dictionaries
+        if not os.path.exists(self.file):
+            print(f"Warning: '{self.file}' not found.")
+            self.contacts = []
+            return self.contacts
 
-        Bonus: What should happen if the file isn't there?
-                What should happen if the file has invalid JSON in it?
-        """
-        return []
+        try:
+            with io.open(self.file, 'r') as file:
+                self.contacts = json.load(file)
+        except json.JSONDecodeError:
+            print(f"Warning: '{self.file}' invalid JSON")
+            self.contacts = []
+            self.save_contacts()
+
+        return self.contacts
+
+
 
     def add_contact(self, contact):
-        """Adds a contact to the list, and saves the file"""
-        pass
+        for saved_contact in self.contacts:
+            if saved_contact['id'] == contact['id']:
+                print('Contact ID already exists.')
+                return
+
+        self.contacts.append(contact)
+        self.save_contacts()
+
+
+
 
     def update_contact(self, contact_to_update):
         """
         Updates a contact an saves the file
-
         Bonus: What happens when the id doesn't exist?
         """
-        pass
+        contacts_to_save = []
+        for contact in self.contacts:
+            if contact['id'] == contact_to_update['id']:
+                contacts_to_save.append(contact_to_update)
+            else:
+                contacts_to_save.append(contact)
+
+        self.contacts = contacts_to_save
+        self.save_contacts()
+
+
 
     def delete_contact(self, id_to_delete):
         """
         Deletes a contact and saves the file
-
         Bonus: What happens when the id doesn't exist?
         """
-        pass
+        contacts_to_save = []
+        for contact in self.contacts:
+            if contact['id'] != id_to_delete:
+                contacts_to_save.append(contact)
+
+        self.contacts = contacts_to_save
+        self.save_contacts()
+
+
+    def save_contacts(self):
+        with io.open(self.file, 'w') as file:
+            json.dump(self.contacts, file)
+

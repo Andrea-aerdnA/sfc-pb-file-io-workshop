8d67e17021d2d0866f14332c74be5636c562e2f3
Riley Dallas <riley.dallas@shortbear.com>
2026-09-01 08:59:54 -0500

    Simplify class

--- contact_manager.py
+++ contact_manager.py
@@ -15,17 +15,24 @@ class ContactManager:
         self.file = file
         # _contacts is protected because we don't want to
         # access it directly from main.py
-        self._contacts: list[dict[str, str]] = []
+        self._contacts = []
 
-    def load_contacts(self) -> list[dict[str, str]]:
-        """Public method to load the contacts into the class"""
-        return self._load_contacts_from_file()
+    def load_contacts(self):
+        """Public method to load the contacts into the class
 
-    def add_contact(self, contact: dict[str, str]) -> None:
+        Loads contacts from a JSON file and converts them to a list of
+        dictionaries
+
+        Bonus: What should happen if the file isn't there?
+                What should happen if the file has invalid JSON in it?
+        """
+        return []
+
+    def add_contact(self, contact):
         """Adds a contact to the list, and saves the file"""
         pass
 
-    def update_contact(self, contact_to_update: dict[str, str]) -> None:
+    def update_contact(self, contact_to_update):
         """
         Updates a contact an saves the file
 
@@ -33,32 +40,10 @@ class ContactManager:
         """
         pass
 
-    def delete_contact(self, id_to_delete: str) -> None:
+    def delete_contact(self, id_to_delete):
         """
         Deletes a contact and saves the file
 
         Bonus: What happens when the id doesn't exist?
-        """
-        pass
-
-    # these are "protected" methods below. They are not meant to be called
-    # from outside the class, but are likely useful in the methods above.
-    def _load_contacts_from_file(self) -> list[dict[str, str]]:
-        """
-        Loads contacts from a JSON file and converts them to a list of
-        dictionaries
-
-        Bonus: What should happen if the file isn't there?
-               What should happen if the file has invalid JSON in it?
-        """
-        return []
-
-    def _save_contacts_to_file(self, contacts: list[dict[str, str]]) -> None:
-        """
-        Converts a list of dictionaries into JSON and writes it to a file
-
-        Bonus: What happens if the file can't be written to?
-               Check the list of dictionaries for valid data before
-               saving it to the file
         """
         pass
